315c1eae8e8a37f0849c56a4b2850baa21128937
Gabriel Nogueira <spbiel18@gmail.com>
2026-08-13 20:19:37 -0300

    refactor: renomeia artifact e classe principal para express-market-api

--- src/main/java/com/br/fiap/express_market/api/ExpressMarketApiApplication.java
+++ src/main/java/com/br/fiap/express_market/api/ExpressMarketApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MercadoApiApplication {
+public class ExpressMarketApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MercadoApiApplication.class, args);
+		SpringApplication.run(ExpressMarketApiApplication.class, args);
 	}
 
 }
